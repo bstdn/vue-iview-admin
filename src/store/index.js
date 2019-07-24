445167c4683c127e745402d98aa06bfaf8f8987b
何秀钢 <xiugang.he@chukou1.com>
2019-07-24 20:23:04 +0800

    vuex

--- src/store/index.js
+++ src/store/index.js
@@ -3,14 +3,16 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
+const modulesFiles = require.context('./modules', true, /\.js$/)
+const modules = modulesFiles.keys().reduce((modules, modulePath) => {
+  const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
+  const value = modulesFiles(modulePath)
+  modules[moduleName] = value.default
+  return modules
+}, {})
 
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
+const store = new Vuex.Store({
+  modules
 })
+
+export default store
