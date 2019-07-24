c281fae1b9d0fc01a57452f2081d9dc741214940
何秀钢 <xiugang.he@chukou1.com>
2019-07-24 20:43:30 +0800

    401、404、500页面

--- src/router/routers.js
+++ src/router/routers.js
@@ -23,5 +23,29 @@ export default [
         component: () => import('@/view/single-page/home')
       }
     ]
+  },
+  {
+    path: '/401',
+    name: 'error_401',
+    meta: {
+      hideInMenu: true
+    },
+    component: () => import('@/view/error-page/401.vue')
+  },
+  {
+    path: '/500',
+    name: 'error_500',
+    meta: {
+      hideInMenu: true
+    },
+    component: () => import('@/view/error-page/500.vue')
+  },
+  {
+    path: '*',
+    name: 'error_404',
+    meta: {
+      hideInMenu: true
+    },
+    component: () => import('@/view/error-page/404.vue')
   }
 ]
---
+++ src/view/error-page/error-content.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="error-page">
+    <div class="content-con">
+      <img :src="src" :alt="code">
+      <div class="text-con">
+        <h4>{{ code }}</h4>
+        <h5>{{ desc }}</h5>
+      </div>
+      <back-btn-group class="back-btn-group" />
+    </div>
+  </div>
+</template>
+
+<script>
+import backBtnGroup from './back-btn-group.vue'
+
+export default {
+  name: 'ErrorContent',
+  components: {
+    backBtnGroup
+  },
+  props: {
+    code: {
+      type: String,
+      default: ''
+    },
+    desc: {
+      type: String,
+      default: ''
+    },
+    src: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="less">
+@import './error.less';
+</style>
